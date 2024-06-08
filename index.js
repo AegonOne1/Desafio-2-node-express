fec57803278c2f6779247150700686085c0a534c
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-06-08 19:11:26 -0400

    Add: realizado el put falta comprobar su funcionamiento de forma correcta

--- index.js
+++ index.js
@@ -37,9 +37,32 @@ app.post("/canciones", (req, res) =>{
 });
 
 //EDITAR
-
+app.put("/canciones/:id", (req,res) =>{
+    try {
+        const id = req.params.id;
+        const cancionEditada = req.body;
+        const canciones = JSON.parse(fs.readFileSync('repertorio.json'));
+        const index = canciones.findIndex(cancionEditada => cancionEditada.id === id);
+        canciones[index] = cancionEditada;
+        fs.writeFileSync('repertorio.json', JSON.stringify(canciones));
+        res.status(200).send('Cancion Modificada con exito')
+    } catch (error) {
+        res.status(500).json({error: 'Error al Modificar la cancion'})
+    };
+});
 
 //BORRAR
+app.delete("/canciones", (req,res) =>{
+    try {
+        res.json({message: 'soy un delete'})
+    } catch (error) {
+        
+    };
+});
 
+//404 not found type
+app.all('*', (req,res) =>{
+    res.status(404).send('Ruta no encontrada')
+})
 
 app.listen(PORT, console.log(`¡Servidor encendido! http://localhost:${PORT}`));
