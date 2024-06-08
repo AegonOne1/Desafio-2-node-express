b62c8fddd69393634357f66e0c411dcef45f79e1
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-06-08 18:41:02 -0400

    add: post creado y funcioanando conectado con html

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const PORT = 3000;
 //MIDDLEWARE
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
 
 //RUTA HOME
 app.get("/", (req, res) =>{
-    res.sendFile(__dirname + '/index.html')
-})
+    res.sendFile(__dirname + '/index.html');
+});
 
 //OBTENER
 app.get("/canciones", (req, res) =>{
@@ -19,14 +20,21 @@ app.get("/canciones", (req, res) =>{
         res.status(200).json(canciones);
     } catch (error) {
         res.status(500).json({error: 'Error al leer el archivo'});
-    }
+    };
 });
 
 //CREAR
 app.post("/canciones", (req, res) =>{
-    res.res('SOY UN POST')
-})
-
+    try {
+        const nuevaCancion = req.body;
+        const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf8'));
+        canciones.push(nuevaCancion);
+        fs.writeFileSync('repertorio.json', JSON.stringify(canciones, null, 2));
+        res.status(201).setDefaultEncoding('Cancion agregada al repertorio');
+    } catch (error) {
+        res.status(500).json({error: 'Error al agregar cancion al archivo'});
+    };
+});
 
 //EDITAR
 
